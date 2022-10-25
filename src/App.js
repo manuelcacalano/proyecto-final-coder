e81207a5c6e9103cabe8ea98229e94948c27c415
manuelcacalano <107509259+manuelcacalano@users.noreply.github.com>
2022-10-25 12:54:59 -0300

    primer preentrega

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Footer from './components/Footer';
 function App() {
   return (
     <>
-      <NavBar color={"secondary"}/>
+      <NavBar />
       <ItemListContainer greeting={"Bienvenido!"} />
       <Footer />
     </>
